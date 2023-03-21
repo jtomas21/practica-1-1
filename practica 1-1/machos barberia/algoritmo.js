b8d34171bce09f9984d051dc4b345aed6f90b9c4
jtomas21 <127454496+jtomas21@users.noreply.github.com>
2023-03-20 23:06:33 -0300

    actualizacion 1

---
+++ practica 1-1/machos barberia/algoritmo.js
@@ -0,0 +1,9 @@
+        document.querySelector("body").innerHTML =`
+        <!-- <h1>Machos Barberia</h1>
+    <h2> </h2>
+    <h3> </h3>
+
+    <img src="https://i.pinimg.com/736x/c2/08/a3/c208a32779077316e4cec719034c481b--barbershop-ideas-barbershop-design-logo.jpg" alt="machos1">
+
+    <img src="imagenes/machos-barber.png" alt=" machos2">
+        `;
